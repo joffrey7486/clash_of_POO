838d0a70198714154d7aa48e87f6cb49e9fb7c02
joffrey7486 <64081938+joffrey7486@users.noreply.github.com>
2021-10-19 17:58:02 +0200

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -3,78 +3,83 @@ class Game
 
     def initialize(name)
         @human_player = HumanPlayer.new(name)
-        player1 = Player.new("player1")
-        player2 = Player.new("player2")
-        player3 = Player.new("player3")
-        player4 = Player.new("player4")
-        @enemies = []
-        @enemies += [player1,player2,player3,player4]
+        @player1 = Player.new("player1")
+        @player2 = Player.new("player2")
+        @player3 = Player.new("player3")
+        @player4 = Player.new("player4")
+        @enemies = Array.new
+        @enemies = [@player1,@player2,@player3,@player4]
     end    
 
 
-    def is_still_ongoing?
-        while @human_player.life_points > 0 || enemies == []
-            menu
-            menu_choice(@human_player,enemies)
-            enemies_attack(@human_player, enemies)
-            show_players
-        end
-            end_game(@human_player)
+    # Retire un player du tableau si il n'a plu de point de vie 
+    def kill_player
+        @enemies.each do |player|
+        if (player.life_points <= 0)
+            @enemies.delete(player)
+            puts "#{player.name} vient d'être éliminé !"
+            end
+        end   
     end
 
+
+    # Vérifie la santé de tout les joueurs et retourne false si le joueus human ou tout les autres n'ont plus de point de vie 
+    def is_still_ongoing?
+        if @human_player.life_points > 0 && (@player1.life_points > 0 || @player2.life_points > 0 || @player3.life_points > 0 || @player4.life_points > 0)
+            return true 
+        else 
+            return false 
+        end         
+    end
+
+    # Retourne le nombre de point de vie du joueur ainsi que le nombre d'ennemis restant
     def show_players
         puts "Tu as #{human_player.life_points}"
         puts "il reste #{@enemies.length} bots, courage!!"
     end
 
+    # Affiche le menu des décisions à prendre pour le joueurs 
     def menu
       puts ""
       puts "Quelle action veux-tu effectuer ?"
       puts "a - chercher une meilleure arme"
       puts "s - chercher à se soigner "
-      puts "attaquer un joueur en vie :"    
+      puts "attaquer un joueur en vie :"   
+      i = 0 
       @enemies.each do |enemy| 
-      puts "#{0} - #{enemy.name} a 10 points de vie"
+      puts ">#{i += 1} pour #{enemy.name} a #{enemy.life_points} points de vie"
       end
     end
     
-    def menu_choice(human_player,enemies)
-        good_choice = true
-        while good_choice == true    
-          choice = gets.chomp 
-          case choice
-              when "a" then human_player.search_weapon
-                good_choice = false
-              when "s" then human_player.search_health_pack
-                good_choice = false
-              when "0" then human_player.attacks(enemies[0])
-                good_choice = false
-              when "1" then human_player.attacks(enemies[1])
-                good_choice = false
-              when "2" then human_player.attacks(enemies[2])
-              good_choice = false
-              when "3" then human_player.attacks(enemies[3])
-              good_choice = false
-          else
-              good_choice = true
-              puts "Tu n'as pas appeler la bonne fonction"
-          end
+    def menu_choice(choice)
+        if choice == "a"
+            @human_player.search_weapon
+        elsif choice == "s"
+            @human_player.search_health_pack
+        elsif choice == "1"
+            @human_player.attacks(@player1)
+        elsif choice == "2"
+            @human_player.attacks(@player2)
+        elsif choice == "3"
+            @human_player.attacks(@player3)
+        elsif choice == "4"
+            @human_player.attacks(@player4)
+        else
+            puts "Tu as le choix entre a, s, 1, 2, 3 ou 4. Recommence !"
         end
+        kill_player
     end
     
-    
-    def enemies_attack(human_player, enemies)
-        enemies.each do |playerX|
-            if playerX.life_points > 0 
-                playerX.attacks(human_player.name)
-            else 
-                kill_player(playerX)
-            end
-        end
+
+    # Utilise la fonction attacks pour chaque ennemis sur le joueur 
+    def enemies_attack
+        puts "Les ennemies vous attaquent"
+        @enemies.each{|player| player.attacks(@human_player)}
     end
     
-    
-    def end_game(human_player)
+
+    # Affiche la fin de la partie ainsi la victoire ou la défaite du joueur
+    def end
       puts "La partie est finie" 
       if human_player.life_points > 0
           puts  "BRAVO ! TU AS GAGNE !"
@@ -82,10 +87,5 @@ class Game
           puts "Loser ! Tu as perdu !"
       end
     end
-
-    def kill_player(playerX)
-        @enemies.delete(playerX)    
-    end
-    
 
 end
